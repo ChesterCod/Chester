c4f9bf8101b5f3f5d03ea42f0336ce6ada3c1a52
ChesterCod <maximelhassan@gmail.com>
2023-07-27 07:58:53 +0300

    JS 1. 2.

---
+++ JS/JS1/main.js
@@ -0,0 +1,89 @@
+let h = "hello";
+console.log(h)
+document.write(h)
+alert(h)
+let o = " owu";
+console.log(o)
+document.write(o)
+alert(o)
+let c = " com";
+console.log(c)
+document.write(c)
+alert(c)
+let u = " ua";
+console.log(u)
+document.write(u)
+alert(u)
+let n1 = " 1";
+console.log(n1)
+document.write(n1)
+alert(n1)
+let n2 = " 10";
+console.log(n2)
+document.write(n2)
+alert(n2)
+let n3 = " -999";
+console.log(n3)
+document.write(n3)
+alert(n3)
+let n4 = " 123";
+console.log(n4)
+document.write(n4)
+alert(n4)
+let n5 = " 3.14";
+console.log(n5)
+document.write(n5)
+alert(n5)
+let n6 = " 2.7";
+console.log(n6)
+document.write(n6)
+alert(n6)
+let n7 = " 16";
+console.log(n7)
+document.write(n7)
+alert(n7)
+let bool1 = " true";
+console.log(bool1)
+document.write(bool1)
+alert(bool1)
+let bool2 = " false";
+console.log(bool2)
+document.write(bool2)
+alert(bool2)
+
+let f = "Максим"
+let m = "Васильович"
+let l = "Метельський"
+
+let r = (f + " " + m + " " + l);
+console.log(r)
+
+let f1 = prompt("Будь ласка, введіть ваше Ім'я:");
+let m1 = prompt("Будь ласка, введіть ваше По-Батькові:");
+let a3 = prompt("Будь ласка, введіть ваш вік:");
+
+console.log("Ім'я: " + f1);
+console.log("По-Батькові: " + m1);
+console.log("Вік: " + a3);
+
+let bo = [
+    {
+        nameBook: "Rich Dad Poor Dad",
+        numberOfPages: 207,
+        genre: "нон-фікшн",
+        authors: ["Роберт Кійосакі", "Шерон Лехтер"]
+    },
+    {
+        nameBook: "Salem's Lot",
+        numberOfPages: 300,
+        genre: "Жахи" + " Містика",
+        author: ["Стівен Кінг"]
+    },
+    {
+        nameBook: " Harry Potter and the Philosopher's Stone",
+        numberOfPages: 319,
+        genre: "фентезі",
+        author: ["Джоан Роулінг"]
+    }
+];
+console.log(bo)
